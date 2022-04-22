61492150903c2609409af91c434288600df4edea
Slav Tomov <stomow@outlook.com>
2022-04-22 12:38:59 +0300

    Add project files.

---
+++ DayOfWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace DayOfWeek
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var date = DateTime.ParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
