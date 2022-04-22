0bfa1a064f4e092afaf8d4fcbea7760cd4c2e429
Slav Tomov <stomow@outlook.com>
2022-04-22 14:34:45 +0300

    Add Big Factorial

---
+++ BigFactorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            BigInteger result = 1;
+            for (int i = 2; i <= n; i++) result *= i;
+
+            Console.WriteLine(result);
+        }
+    }
+}
