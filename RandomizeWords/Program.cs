339308c51465766f30aa26fa580e689ec5cd3fe4
Slav Tomov <stomow@outlook.com>
2022-04-22 14:21:37 +0300

    Add Randomize Words

---
+++ RandomizeWords/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace RandomizeWords
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var words = Console.ReadLine().Split(' ').ToArray();
+            var rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(words.Length);
+                string word = words[pos1];
+                words[pos1] = words[pos2];
+                words[pos2] = word;
+            }
+
+            Console.WriteLine(String.Join("\r\n", words));
+        }
+    }
+}
